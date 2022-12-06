d0e64895e3ad93d0754a8ca3639f80ea50fe2345
Linda Attara <lindaattara49@gmail.com>
2022-12-06 10:28:46 -0800

    number of arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 2-args.py
-#NanaYalin <lindaattara49@gmail.com>
+# NanaYalin <lindaattara49@gmail.com>
 
 if __name__ == "__main__":
     """Print the number of and list of arguments."""
